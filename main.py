0e1f68c69863fbbb69e34ae4b2728f32342ed377
Anush P <av.pogosyan@gmail.com>
2022-04-24 13:59:54 -0700

    get env values

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ import logging
 import datetime
 import functools
 import jwt
+from dotenv import load_dotenv
 
 # pylint: disable=import-error
 from flask import Flask, jsonify, request, abort
 
- 
-JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
-LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
+load_dotenv()
+
+
+JWT_SECRET = os.environ.get('JWT_SECRET') #, 'abc123abc1234')
+LOG_LEVEL = os.environ.get('LOG_LEVEL') #, 'INFO')
 
 
 def _logger():
