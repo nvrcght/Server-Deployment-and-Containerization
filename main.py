17ea52940af467b769f34057d78d0383e1f34c68
Anush P <av.pogosyan@gmail.com>
2022-04-24 15:40:06 -0700

    added default env variables

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ from flask import Flask, jsonify, request, abort
 load_dotenv()
 
 
-JWT_SECRET = os.environ.get('JWT_SECRET') #, 'abc123abc1234')
-LOG_LEVEL = os.environ.get('LOG_LEVEL') #, 'INFO')
+JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
+LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
 
 def _logger():
